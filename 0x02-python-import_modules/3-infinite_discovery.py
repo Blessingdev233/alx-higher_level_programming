ce73ab9adc26568fe939da124ac4abe274740502
Blessingdev233 <blessing.lartey@gmail.com>
2022-06-02 19:47:09 +0000

    correction

--- 0x02-python-import_modules/3-infinite_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
-    total = 0
-    argc = len(sys.argv)
-    for i in range(1, argc):
-        total += int(sys.argv[i])
-    print(total)
