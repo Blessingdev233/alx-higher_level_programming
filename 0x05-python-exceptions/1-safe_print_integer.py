24fae1c2451057678e0f869b3e8e519fd232a28f
Blessingdev233 <blessing.lartey@gmail.com>
2022-06-20 21:13:57 +0000

    Prints an integer with a format

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
-        return False
+   except (TypeError, ValueError):
+        return (False)
