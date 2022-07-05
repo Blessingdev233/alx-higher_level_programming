a0adf621cfd2be2de989d662eb49fdb834a79597
Blessingdev233 <blessing.lartey@gmail.com>
2022-07-05 09:12:53 +0000

    For an instance of a specified class,returns a boolean

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-        """ same class """
-        if isinstance(obj, (a_class)) and type(obj) is a_class:
-                return True
-        return False
+  """ same class """
+    if isinstance(obj, (a_class)) and type(obj) is a_class:
+        return True
+    return False
