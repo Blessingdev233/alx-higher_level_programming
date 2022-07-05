6a17650c44cad5d9dd6c0cac96559ce1d35d6725
Blessingdev233 <blessing.lartey@gmail.com>
2022-07-05 14:23:24 +0000

    fixing

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-""" start doc """
+"""contains pascal_triangle"""
 
 
 def pascal_triangle(n):
-    """ interview question """
-    l = []
+    """Generates the pascal triangle"""
     if n <= 0:
-        return l
-    for a in range(1, n+1):
+        return []
+    pascal = []
+    for i in range(n):
         temp = []
-        for v in range(0, a):
-            temp.append(1)
-        l.append(temp)
-
-    for i in range(1, n):
-        for b in range(i, 0, -1):
-            l[i][b - 1] = l[i - 1][b - 2] + l[i - 1][b - 1]
-            l[i][i] = 1
-            l[i][0] = 1
-    return l
+        for j in range(n):
+            temp.append(0)
+        pascal.append(temp)
+    for i in range(1, n + 1):
+        for j in range(i):
+            if j == 0:
+                pascal[i - 1][j] = 1
+            else:
+                pascal[i - 1][j] = pascal[i - 2][j] + pascal[i - 2][j - 1]
+    formatted = []
+    for row in range(1, n + 1):
+        temp = []
+        for col in range(row):
+            temp.append(pascal[row - 1][col])
+        formatted.append(temp)
+    return formatted
