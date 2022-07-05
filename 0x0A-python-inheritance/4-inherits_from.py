c5427f7fb5c8779cbbce0deb033d1e7629e1fd5e
Blessingdev233 <blessing.lartey@gmail.com>
2022-07-05 09:07:26 +0000

    function that returns a boolean

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-        """inherit"""
-        if issubclass(type(obj), (a_class)) and type(obj) is not a_class:
-                return True
-        return False
+    """inherit"""
+    if issubclass(type(obj), (a_class)) and type(obj) is not a_class:
+        return True
+    return False
