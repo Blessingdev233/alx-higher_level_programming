d98f48b0790164fc5d2260291354bdcf2177d220
Blessingdev233 <blessing.lartey@gmail.com>
2022-08-09 18:54:58 +0000

    Best Score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,6 @@
 -- lists all records with a score >= 10 in the table `second_table` of the database `hbtn_0c_0` in MySQL server.
-SELECT *
+SELECT score, name
     FROM second_table
     WHERE score >= 10
+    ORDER BY score DESC;
+    ORDER BY `score` DESC;
