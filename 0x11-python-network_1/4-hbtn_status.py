8a5749701e64be0d254d3894aea431e145fd2c27
Blessingdev233 <blessing.lartey@gmail.com>
2022-09-23 20:55:50 +0000

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-from requests import get
-
-
 if __name__ == "__main__":
-    request = get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(request.text)))
-    print("\t- content: {}".format(request.text))
+    import requests
 
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+          .format(type(r.text), r.text))
